de44a5e63d9c7b327fd002be1561663f6918e8b3
Baptiste Esteban <baptiste.esteban@epita.fr>
2019-11-21 11:38:06 +0100

    [OBJECT]: add abstract object class

---
+++ src/object.hh
@@ -0,0 +1,16 @@
+#ifndef OBJECT_HH
+# define OBJECT_HH
+
+# include <SDL2/SDL.h>
+
+# include "camera.hh"
+# include "mat4f.hh"
+
+class Object
+{
+public:
+  virtual void draw(const Camera& cam, const mat4f& proj) = 0;
+  virtual void update(const SDL_Event& e) = 0;
+};
+
+#endif /* !OBJECT_HH */
