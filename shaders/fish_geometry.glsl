bf0969d39578b16ef5a564252f225a0897674765
Charles Ginane <cginane@lrde.epita.fr>
2019-12-12 09:56:20 +0100

    GEOMETRY: add geometry shader for fish

--- shaders/fish_geometry.glsl
+++ shaders/fish_geometry.glsl
@@ -1,15 +1,51 @@
 #version 450
 
 layout(points) in;
-layout(points, max_vertices = 2) out;
+layout(triangle_strip, max_vertices = 20) out;
 
+void draw_fish(float offset) {
+gl_Position = gl_in[0].gl_Position;
+  EmitVertex();
+  gl_Position   = gl_in[0].gl_Position + offset;
+  gl_Position.y = gl_Position.y - 0.1;
+  EmitVertex();
+  gl_Position   = gl_in[0].gl_Position + offset;
+  gl_Position.y = gl_Position.y + 0.1;
+  EmitVertex();
+  gl_Position   = gl_in[0].gl_Position + offset;
+  gl_Position.x = gl_Position.x + 0.15;
+  EmitVertex();
+  gl_Position   = gl_in[0].gl_Position + offset;
+  gl_Position.y = gl_Position.y + 0.1;
+  EmitVertex();
+  gl_Position   = gl_in[0].gl_Position + offset;
+  gl_Position.y = gl_Position.y - 0.1;
+  EmitVertex();
+  gl_Position   = gl_in[0].gl_Position + offset;
+  gl_Position.x = gl_Position.x - 0.15;
+  EmitVertex();
+
+  gl_Position = gl_in[0].gl_Position + offset;
+  EmitVertex();
+  gl_Position   = gl_in[0].gl_Position + offset;
+  gl_Position.x = gl_Position.x + 0.1;
+  EmitVertex();
+  gl_Position   = gl_in[0].gl_Position + offset;
+  gl_Position.x =gl_Position.x + 0.1;
+  EmitVertex();
+
+  gl_Position   = gl_in[0].gl_Position + offset;
+  gl_Position.x =gl_Position.x + 0.2;
+  gl_Position.y = gl_Position.y + 0.1;
+  EmitVertex();
+  gl_Position   = gl_in[0].gl_Position + offset;
+  gl_Position.x = gl_Position.x + 0.2;
+  gl_Position.y = gl_Position.y - 0.1;
+  EmitVertex();
+}
 
 void main()
 {
-  gl_Position = gl_in[0].gl_Position;
-  EmitVertex();
-  gl_Position   = gl_in[0].gl_Position;
-  gl_Position.y = -1 * gl_in[0].gl_Position.y;
-  EmitVertex();
+  draw_fish(0);
   EndPrimitive();
 }
