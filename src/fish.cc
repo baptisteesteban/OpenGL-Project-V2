2b079cda39ef75523ccf0ad0c6a04365e550bb27
Thomas De Carvalho <thomas.decarvalho@epita.fr>
2020-02-07 02:56:33 +0100

    final

--- src/fish.cc
+++ src/fish.cc
@@ -39,7 +39,7 @@ void Fish::update(const SDL_Event& e)
 {
   auto new_time = SDL_GetTicks();
   auto fps      = new_time - old_time_;
-  //cs_.use(elem_.size(), 1, 1);
+  cs_.use(elem_.size(), 1, 1);
   cs_.setUniformUnsigned("fps", fps);
   cs_.setUniformUnsigned("size", elem_.size());
   old_time_ = new_time;
